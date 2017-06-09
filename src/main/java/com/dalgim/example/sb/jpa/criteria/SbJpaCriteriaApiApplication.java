202f2112807907e5d6fb72317ff1030fd6affd16
dalgim <dalgiewiczmateusz@gmail.com>
2017-06-09 22:48:01 +0200

    Initial commit

---
+++ src/main/java/com/dalgim/example/sb/jpa/criteria/SbJpaCriteriaApiApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.jpa.criteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaCriteriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaCriteriaApiApplication.class, args);
+	}
+}
